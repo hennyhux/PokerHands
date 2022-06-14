c1931c8d1ae92fe09f11ce07d0147d1a5f126d66
Henry Zhang <hennyhux@gmail.com>
2022-06-14 18:26:48 -0400

    Added new hands

--- src/Poker.java
+++ src/Poker.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Poker {
 
     public static void main(String args[]){
-        Card newCard = new Card(Suits.CLUBS, Values.ACE);
-        System.out.println(newCard.getValue().value);
+        firstTestCase();
 
+    }
+
+    public static void firstTestCase(){
+        ArrayList<Card> blackHand = new ArrayList<>(Arrays.asList
+                (new Card(Suits.HEARTS, Values.TWO),
+                 new Card(Suits.DIAMONDS, Values.THREE),
+                 new Card(Suits.SPADES, Values.FIVE),
+                 new Card(Suits.CLUBS, Values.NINE),
+                 new Card(Suits.DIAMONDS, Values.KING)
+                ));
+
+        Hand firstHand = new Hand(blackHand);
+
+        ArrayList<Card> whiteHand = new ArrayList<>(Arrays.asList
+                (new Card(Suits.CLUBS, Values.TWO),
+                        new Card(Suits.HEARTS, Values.THREE),
+                        new Card(Suits.SPADES, Values.FOUR),
+                        new Card(Suits.CLUBS, Values.EIGHT),
+                        new Card(Suits.HEARTS, Values.ACE)
+                ));
+
+        Hand secondHand = new Hand(whiteHand);
     }
 
 }
